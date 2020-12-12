5fa20c1d234b4a33b427e34ad01e57bd37696f5b
Alexandr Smirnov <alexsm.dev@gmail.com>
2020-12-12 21:33:22 +0300

    added selector for Info

--- src/Info.jsx
+++ src/Info.jsx
@@ -3,7 +3,6 @@ import {connect} from "react-redux";
 
 
 const Info = memo(({info: {title}}) => {
-    console.log(title)
     useEffect(() => console.log('render Info'))
     return (
         <div>
@@ -13,8 +12,12 @@ const Info = memo(({info: {title}}) => {
     )
 })
 
+const getInfoSelector = state => state.fetchStatus.info
+
 const mapStateToProps = state => ({
-    info: state.fetchStatus.info
+    // info: state.fetchStatus.info
+    // info: state
+    info: getInfoSelector(state)
 })
 
 export default connect(mapStateToProps, null)(Info)
